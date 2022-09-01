d7ba6a8ca99f5ea0947fa223a98f9a8ff6437d74
Mikita Radzkou <mikita.radzkou@boweryvaluation.com>
2022-09-01 19:21:19 +0300

    Added schema

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const {graphqlHTTP} = require('express-graphql')
 const cors = require('cors')
+const schema = require('./schema')
 
 const PORT = process.env.PORT || 5500
 
@@ -8,7 +9,8 @@ const app = express()
 app.use(cors())
 
 app.use('/graphql', graphqlHTTP({
-    graphiql: true
+    graphiql: true,
+    schema
 }))
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
