e3c8ae1f4e95c90d6b379e78a23c5b7f0c8f16cd
Mikita Radzkou <mikita.radzkou@boweryvaluation.com>
2022-09-01 18:52:14 +0300

    Added middleware to index.js

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express')
+const {graphqlHTTP} = require('express-graphql')
+const cors = require('cors')
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5500
 
 const app = express()
+app.use(cors())
+
+app.use('/graphql', graphqlHTTP({
+    graphiql: true
+}))
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
